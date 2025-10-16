6802e0fdf40a00766fd555915b6042e96b9d62e2
TPalatan <palatanteody79@gmail.com>
2025-10-17 03:20:07 +0800

    Revert "study view design"

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import StudyScreen from './StudyScreen/StudyScreen';
+import ProfileScreen from './Profile/ProfileScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-    <StudyScreen/>
+      <ProfileScreen/>
       <StatusBar style="auto" />
     </View>
   );
